b02b2990cbbebc4d2acea7edc27231f38c4e9d1a
Jobayer-Ahmed <jobayeramickey@gmail.com>
2018-06-26 03:38:18 +0600

    initial commit

---
+++ module.js
@@ -0,0 +1,59 @@
+// Declaring global variables
+let x = 0, number;
+const date = new Date();
+
+// Main function
+function getDateFromString(str) {
+	// Splitting input
+	let split = str.split(' '), firsttext = split[1];
+	// check if input's first letter contain 'a' or 'an'
+	if (split[0] === 'a' || split[0] === 'an') {
+		number = 1;
+	} else {
+		number = parseInt(split[0]);
+	}
+
+	// Get actual number from first text input
+	if (firsttext === "week" || firsttext === "weeks") {
+		x = number*7;
+		getDate(x)
+	} else if (firsttext === "month" || firsttext === "months"){
+		x = number*30;
+		getDate(x)
+	} else if (firsttext === "day" || firsttext === "days") {
+		x = number*1
+		getDate(x)
+	} else {
+		getTime(number, firsttext);
+	}
+}
+
+// Get time if input doesn't contain any day, week, month
+function getTime(int, str) {
+	let datetime = '';
+	if (str === "second" || str === "seconds") {
+		x = int*1000
+		datetime = new Date(date - x)
+	} else if (str === "minute" || str === "minutes") {
+		x = int*60*1000
+		datetime = new Date(date - x)
+	} else if (str === "hour" || str === "hours") {
+		x = (int*60)*60*1000
+		datetime = new Date(date - x)
+	} else {
+		return undefined
+	}
+	let timeAndDateOnly = `${datetime.getDate()}-${datetime.getMonth()}-${datetime.getFullYear()} ${datetime.getHours()}:${datetime.getMinutes()}:${datetime.getSeconds()}`;
+	return timeAndDateOnly
+}
+
+// Get time if input doesn't contain any second, minute, hour
+function getDate(int) {
+	let mainDate = date.setDate(date.getDate() - int);
+	var dateMsg = date.getDate()+'/'+ (date.getMonth()+1) +'/'+date.getFullYear() + ` ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+	return dateMsg
+}
+
+
+// exporting module
+module.exports = getDateFromString;
